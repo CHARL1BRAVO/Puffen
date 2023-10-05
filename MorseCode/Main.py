9ee247a62b7773bcbc4fe5f3d52f3002cf1df5dd
CHARL1BRAVO <90268325+CHARL1BRAVO@users.noreply.github.com>
2023-10-05 19:18:31 +0200

    Add

---
+++ MorseCode/Main.py
@@ -0,0 +1,25 @@
+# Define Morse code mappings
+morse_code_dict = {
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....', 
+    'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 
+    'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 
+    'Y': '-.--', 'Z': '--..',
+    '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....',
+    '6': '-....', '7': '--...', '8': '---..', '9': '----.',
+    ' ': ' '
+}
+
+def text_to_morse(text):
+    morse_code = []
+    for char in text.upper():
+        if char in morse_code_dict:
+            morse_code.append(morse_code_dict[char])
+        else:
+            morse_code.append(' ')
+    
+    return ' '.join(morse_code)
+
+if __name__ == "__main__":
+    user_input = input("Enter a string: ")
+    morse_code = text_to_morse(user_input)
+    print(f"Morse Code: {morse_code}")
