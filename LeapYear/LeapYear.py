7c776f88fcb22efbc421b7c49f449c36a437ad51
CHARL1BRAVO <90268325+CHARL1BRAVO@users.noreply.github.com>
2023-10-18 23:51:12 +0200

    projects

--- LeapYear/LeapYear.py
+++ LeapYear/LeapYear.py
@@ -1,19 +1,11 @@
-def is_leap(year)
-  if year % 4 == 0:
-    if year % 100 == 0:
-      if year % 400 == 0:
-        print("Leap year")
-      else:
-        print("Not leap year")
+def is_leap_year(year):
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+        return True
     else:
-      print("Leap year")
-  else:
-    print("Not leap year")
-  
-def days_in_month():
-  month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31] 
+        return False
 
-year = int(input()) # Enter a year
-month = int(input()) # Enter a month
-days = days_in_month(year, month)
-print(days)
+# Example usage:
+if is_leap_year(year):
+    print(f"{year} is a leap year")
+else:
+    print(f"{year} is not a leap year")
